20e92d1cdb33ca342281f183c518971f5b945e70
Andrzej Zabski <jedrak32@gmail.com>
2019-11-06 23:32:54 +0100

    jacob stuff

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class playerMovement : MonoBehaviour
 {
 
@@ -12,8 +14,10 @@ public class playerMovement : MonoBehaviour
 
     public GameObject bulletPrefab;
     public float bulletSpeed;
+    public int magazinesize=6;
     private float lastFire;
     public float FireDelay;
+    public float ReloadDelay=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,14 +32,28 @@ public class playerMovement : MonoBehaviour
         rb.velocity = playerInput.normalized * moveSpeed;
         float shootHor = Input.GetAxisRaw("Horizontalshoot");
         float shootVert = Input.GetAxisRaw("Verticalshoot");
-        if((shootHor!=0||shootVert!=0) && Time.time>lastFire +FireDelay)
+        if((shootHor!=0||shootVert!=0) && Time.time>lastFire +FireDelay+ ReloadDelay)
         {
-            Shoot(shootHor, shootVert);
+            ReloadDelay = 0;
+            if (magazinesize>0)
+            {
+                Shoot(shootHor, shootVert);
+                magazinesize--;
+               
+            }
+            else
+            {
+                
+                FindObjectOfType<AudioManager>().Play("reload");
+                magazinesize = 6;
+                ReloadDelay = 0.5f;
+            }
             lastFire = Time.time;
         }
     }
     void Shoot(float x, float y)
     {
+        int rand = Random.Range(1, 7);
         float shotAngle = Vector3.Angle(new Vector3(x, y), Vector3.right);
         GameObject bullet;
         if (y < 0)
@@ -51,6 +69,8 @@ public class playerMovement : MonoBehaviour
         bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
             (x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
               (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed);
+        FindObjectOfType<AudioManager>().Play("gunshot"+rand);
+       
 
     }
 }
