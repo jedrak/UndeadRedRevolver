57c5fcf7ccb1b011fbaad4799264b46fca24c5df
jedrak <jedrak32@gmail.com>
2020-01-15 13:23:22 +0100

    greyscale shader

---
+++ Assets/Scripts/PostProcess.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcess : MonoBehaviour
+{
+    public Material mat;
+    public TimerManager tm;
+   
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
+    }
+
+    private void Update() {
+        if(tm._playerIsDead){
+            mat.SetInt("playerIsDead", 1);
+
+        }
+        else
+        {
+            mat.SetInt("playerIsDead", 0);
+        }
+       
+    }
+}
---
+++ Assets/Scripts/RoomUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RoomUI : MonoBehaviour
+{
+    public Map map;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TextMeshProUGUI>().text = "X: " + map._playerX + " Y: " + map._playerY;
+    }
+}
