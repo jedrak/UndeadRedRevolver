50448f09e0a4d1b4244e3705b4eba8adfb6338b2
Jarosław Suchiński <ven.cjin@gmail.com>
2020-01-30 00:29:25 +0100

    Score deleted

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -10,9 +10,7 @@ public class TimerManager : MonoBehaviour
     public bool _playerIsDead { get; set; }
     public bool _playerStateChanged { private get; set; }
     public int monsterKillcount { get; set; }
-    public int score { get; set; }
     public GameObject killCountText;
-    public GameObject scoreText;
     public GameObject messageText;
     public GameObject gameplayInfoText;
     public RespawnController respawnController;
@@ -23,24 +21,27 @@ public class TimerManager : MonoBehaviour
         _playerIsDead = true;
         mat.SetFloat("blendDuration", 2.0f);
         monsterKillcount = 0;
-        score = 0;
         gameplayInfoText.GetComponent<TextMeshProUGUI>().text = "Move with WSAD, shoot with Arrows Keys";
     }
 
     void FixedUpdate()
     {
-        if (Time.time > 10) gameplayInfoText.GetComponent<TextMeshProUGUI>().text = "";
+        if (Time.time > 10) gameplayInfoText.GetComponent<TextMeshProUGUI>().text = ""; // 10 sekund
 
         killCountText.SetActive(_playerIsDead);
-        scoreText.SetActive(!_playerIsDead);
-        if (_playerIsDead)
+
+        if (Time.time > 120) // 2 min
         {
-            messageText.GetComponent<TextMeshProUGUI>().text = "You are dead.\nKill enemies to get more alive time!";
+            if (_playerIsDead)
+            {
+                messageText.GetComponent<TextMeshProUGUI>().text = "You are dead.\nKill enemies to get more alive time!";
+            }
+            else
+            {
+                messageText.GetComponent<TextMeshProUGUI>().text = "You are alive.\nCollect items in chests and explore rooms!";
+            }
         }
-        else
-        {
-            messageText.GetComponent<TextMeshProUGUI>().text = "You are alive.\nCollect items in chests and explore rooms!";
-        }
+
         if (timer.timeToEnd < 0)
         {
             FindObjectOfType<AudioManager>().Play("morde");
@@ -49,25 +50,18 @@ public class TimerManager : MonoBehaviour
             _playerStateChanged = true;
             mat.SetFloat("startTime", Time.time);
         }
+
         if (_playerIsDead)
         {
-
             timer.timeToEnd = monsterKillcount * 2;
-            score = 0;
-
         }
         else
         {
-
-
-            //timer.timeToEnd = 10 + 2 * monsterKillcount;
             monsterKillcount = 0;
-
         }
 
         foreach (Transform t in room.GetComponentInChildren<Transform>())
         {
-            //Debug.Log(t.gameObject, this);
             if (t.gameObject.tag == "Spawner")
             {
                 t.gameObject.SetActive(_playerIsDead);
@@ -93,9 +87,5 @@ public class TimerManager : MonoBehaviour
             }
             _playerStateChanged = false;
         }
-
-
-
-
     }
 }
