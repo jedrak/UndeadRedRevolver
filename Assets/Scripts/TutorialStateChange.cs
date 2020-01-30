ec7c7e58d1cdb91f4870471cc625e0e6359edbe7
Andrzej Zabski <jedrak32@gmail.com>
2020-01-30 08:45:44 +0100

    tutorial poczatki

---
+++ Assets/Scripts/TutorialStateChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialStateChange : MonoBehaviour
+{
+    public TimerManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (manager._playerIsDead)
+        {
+            manager._playerIsDead = false;
+            manager.timer.timeToEnd = 500;
+        }
+        else
+        {
+            manager._playerIsDead = true;
+            manager.timer.timeToEnd = 0;
+        }
+        Debug.Log(collision.gameObject.tag);
+    }
+}
